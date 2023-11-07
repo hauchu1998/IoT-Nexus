7a96753510c1a357bc8c4900b22e7866412b89fe
Maureen Hsu <maureen.hsu@gmail.com>
2023-11-07 00:59:00 -0500

    gen_compact_certificate

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse, Response
 from fastapi.middleware.cors import CORSMiddleware
 
-
+from py_cc.cc import gen_compact_certificate
 from app import db
 from app import utils
 from app.models import (
@@ -70,53 +70,37 @@ async def startup_event():
 
 @app.post("/api/genProof")
 async def genproof(request: GenProofRequest):
-    command = (
-        "python3 main.py -n "
-        + str(request.count)
-        + " -m "
-        + str(request.message)
+    message = db.messages[request.message]
+    signed_attestors = {}
+    for validator, signature in message.signed_validators.items():
+        attestor = db.get_attestor(validator.wallet_address)
+        sig_cc = db.get_signature_cc(message.message, validator)
+        signed_attestors[attestor] = sig_cc
+    gen_compact_certificate(
+        message=message.message,
+        attestors=list(db.attestors.values()),
+        signed_attestors=signed_attestors,
     )
-    result = subprocess.run(command, shell=True, capture_output=True)
-    genCertTime = result.stdout.decode("utf-8").strip().split(" ")[-1]
 
     # generate zkproof
     command_zoc = "cd zokratesjs && node index.js && cd .."
-    zoc_result = subprocess.run(
-        command_zoc,
-        shell=True,
-        capture_output=True).stdout.decode('utf-8')
-    compileZokTime, computeTime, verifyTime, _ = [s.strip().split(
-        " ")[-1] for s in zoc_result.split('\n')]
-
-    # with open("certificate.json") as f:
-    #     certificate = json.load(f)
+    try:
+        subprocess.run(
+            command_zoc,
+            shell=True,
+            capture_output=True).stdout.decode('utf-8')
+    except subprocess.CalledProcessError as e:
+        return Response(status_code=500, content=e.stderr)
 
     with open("./zokratesjs/proof.json") as f:
         proof = json.load(f)
-
-    # with open("attest.txt") as f:
-    #     lines = f.read().splitlines()
-    #     validators = []
-    #     for count, line in enumerate(lines):
-    #         if count < int(request.count):
-    #             public_key, weight = line.split(",")
-    #             validators.append({"public_key": public_key, "weight": weight})
-
-    # data = GenProofResponse(**{
-    #     "certificate": certificate,
-    #     "proof": proof,
-    #     "validators": validators,
-    #     "genCertTime": genCertTime + " sec",
-    #     "compileZokTime": compileZokTime,
-    #     "computeTime": computeTime,
-    #     "verifyTime": verifyTime
-    # })
 
     return JSONResponse(content=proof)
 
 
 @app.post("/api/signMessage")
 async def sign_message(request: SignMessageRequest):
+    validator = db.get_validators(request.wallet_address)
     attestor = db.get_attestor(request.wallet_address)
     validator = db.get_validators(request.wallet_address)
 
@@ -184,59 +168,3 @@ async def get_messages() -> List[dict]:
 async def save_message(request: SaveMessageRequest):
     db.save_message(message=request.message,
                     wallet_address=request.wallet_address)
-
-
-# @app.post("/upload")
-# async def upload(request: Request):
-#     input_data = await request.json()
-#     input_count = input_data["inputCount"]
-#     input_message = input_data["inputMessage"]
-#     print(input_count, input_message)
-
-#     run_command = (
-#         "python3 main.py -n "
-#         + str(input_count)
-#         + " -m "
-#         + str(input_message)
-#     )
-#     print(run_command)
-#     result = subprocess.run(run_command, shell=True, capture_output=True)
-#     print(result)
-#     genCertTime = result.stdout.decode("utf-8").strip().split(" ")[-1]
-
-#     # generate zkproof
-#     run_command_zoc = "cd zokratesjs && node index.js && cd .."
-
-#     zoc_result = subprocess.run(
-#         run_command_zoc,
-#         shell=True,
-#         capture_output=True).stdout.decode('utf-8')
-#     compileZokTime, computeTime, verifyTime, _ = [s.strip().split(
-#         " ")[-1] for s in zoc_result.split('\n')]
-
-#     print(compileZokTime, computeTime, verifyTime)
-
-#     with open("certificate.json") as f:
-#         data1 = json.load(f)
-
-#     with open("./zokratesjs/proof.json") as f:
-#         data2 = json.load(f)
-
-#     with open("attest.txt") as f:
-#         lines = f.read().splitlines()
-#         data3 = []
-#         for count, line in enumerate(lines):
-#             if count < int(input_count):
-#                 public_key, weight = line.split(",")
-#                 data3.append({"public_key": public_key, "weight": weight})
-#     data = {
-#         "data1": data1,
-#         "data2": data2,
-#         "data3": data3,
-#         "genCertTime": genCertTime + " sec",
-#         "compileZokTime": f"Compile Zokrates: {compileZokTime} sec",
-#         "computeTime": f"Compute Witness & Generate Proof: {computeTime} sec",
-#         "verifyTime": f"Verify Proof: {verifyTime} sec"
-#     }
-
-#     return JSONResponse(content=data)
